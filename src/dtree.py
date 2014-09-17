87c33b4a37033307a7d3b78856610d9a6ed120ff
Greg Ziegan <greg.ziegan@gmail.com>
2014-09-17 13:03:57 -0400

    depth configuration

--- src/dtree.py
+++ src/dtree.py
@@ -1,14 +1,16 @@
 import mldata as md
 import numpy as np
 import argparse
-
+from tree_generation import DecisionTree
 
 def main(problem_name, max_depth):
     dataset = md.parse_c45(problem_name)
     data_array = np.array(dataset.to_float())
     np.random.shuffle(data_array)
     training_set = data_array[:4 * len(data_array)/5]
-
+    validation_set = data_array[4 * len(data_array)/5:]
+    dtree = DecisionTree(training_set, dataset.schema, max_depth=max_depth)
+    print dtree.root
 
 
 
@@ -18,6 +20,6 @@ if __name__ == '__main__':
                    help='The name of the problem set to process.')
     parser.add_argument('--max-depth', action='store_const',
                    help='Limit the tree to a certain depth.')
-
+    #TODO check if depth > 0
     args = parser.parse_args()
     main(args.problem_name, args.max_depth)
--- src/tree_generation.py
+++ src/tree_generation.py
@@ -1,5 +1,9 @@
-import utils
+# coding: utf-8
+import datetime
+import pydot
+
 from feature_selection import get_best_feature, generate_feature_test
+import utils
 
 
 class DecisionTreeNode(object):
@@ -22,22 +26,21 @@ class DecisionTreeNode(object):
 
 class DecisionTree(object):
 
-    def __init__(self, examples, target_feature, features, max_depth):
-        self.root = self._generate_tree(examples, target_feature, features, 0)
+    def __init__(self, examples, features, max_depth):
+        self.root = self._generate_tree(examples, features, 0)
         self.max_depth = max_depth
 
-    def _generate_tree(self, examples, target_feature, features, depth, parent=None):
+    def _generate_tree(self, examples, features, depth, parent=None):
         root = DecisionTreeNode(parent=parent)
         parent.add_child(root)
 
         if not examples:
             raise Exception("No examples provided. ID3 failed.")
 
-        if depth == self.max_depth:
+        if self.max_depth > 0 and depth == self.max_depth:
             leaf = DecisionTreeNode(parent=root, label=utils.most_common_value(examples))
             root.add_child(leaf)
             return root
-
 
         if utils.is_homogeneous(examples, positive=True): # test if all examples for class label are positive
             root.label = True
@@ -62,10 +65,14 @@ class DecisionTree(object):
                 features_without_best_classifier = [feature for feature in features if feature != best_feature]
                 self._generate_tree(
                     examples=examples_matching_feature_value,
-                    target_feature=target_feature,
                     features=features_without_best_classifier,
-                   depth=depth + 1,
+                    depth=depth + 1,
                     parent=node,
                 )
 
         return root
+
+
+def print_tree(decision_tree):
+    graph = pydot.Dot(graph_type='graph# the idea here is not to cover how to represent the hierarchical data')
+    graph.write_png('decision_tree_{}.png'.format(str(datetime.datetime.now())))
